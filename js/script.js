460309d144b783bdb3f7c7ee18aad509fd030400
Isabelle Sayuri Isa <isabelle.say.isa@gmail.com>
2025-04-07 17:39:06 -0300

    Coloquei uma seta ae

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.sub-list').forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('open');
+
+        const subList = item.querySelector('.sub-list-ul');
+        const icon = item.querySelector('.fa-chevron-up');
+
+        if (subList) {
+            subList.classList.toggle('hidden');
+        }
+
+        if (icon) {
+            icon.classList.toggle('open');
+        }
+    });
+});
